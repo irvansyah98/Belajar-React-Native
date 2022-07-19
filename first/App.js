a569e09cfb982bce39f4eb23af6f1c1445e1e2a9
iervan <iervanfirdiansyah1998@gmail.com>
2022-07-19 11:02:28 +0700

    first commit

---
+++ first/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+export default App;

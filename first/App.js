be25fdc8057db3a4897fbc1ba40718afda52e06e
iervan <iervanfirdiansyah1998@gmail.com>
2022-07-19 13:55:53 +0700

    stylesheet

--- first/App.js
+++ first/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Text, View, StatusBar, Image, TouchableOpacity, Switch, TextInput} from 'react-native';
+import {Text, View, StatusBar, Image, TouchableOpacity, Switch, TextInput, ScrollView, StyleSheet} from 'react-native';
 
 class App extends Component {
   constructor(props) {
@@ -12,27 +12,21 @@ class App extends Component {
   }
   render() {
     return (
-      <View style={{flex:1}}>
+      <ScrollView style={{flex:1}}>
       <StatusBar barStyle="light-content" backgroundColor="#e53935"/>
-        <View style={{
-          backgroundColor:'#c62828',
-          paddingVertical:20,
-          justifyContent: 'center',
-          alignItems:'center',
-          elevation:10
-        }}>
-          <Text style={{color:'#FFFFFF',fontWeight:'bold'}}>
+        <View style={styles.textheaderContainer}>
+          <Text style={styles.textheader}>
             {this.state.header}
           </Text>
         </View>
-        <View style={{justifyContent:'center',alignItems:'center'}}>
+        <View style={styles.imageContainer}>
           <Image 
             source={require('./src/images/image1.jpg')}
-            style={{width:300,height:300}}
+            style={styles.image}
           />
         </View>
 
-        <View style={{justifyContent:'center',alignItems:'center',marginTop:20}}>
+        <View style={styles.switch}>
           <Switch
             value={this.state.value}
             onValueChange={() => this.setState({value : !this.state.value})}
@@ -41,25 +35,59 @@ class App extends Component {
 
         <TextInput
           value={this.state.username}
-          style={{borderBottomWidth:1, marginHorizontal: 20, paddingVertical:10, borderColor:'#c62828'}}
+          style={styles.textinput}
           onChangeText={(value) => this.setState({username:value})}
         />
 
-        <TouchableOpacity style={{
-          backgroundColor:'#c62828',
-          paddingVertical:20,
-          justifyContent:'center',
-          alignItems:'center',
-          marginTop:20,
-          marginHorizontal:20,
-          elevation:3,
-          borderRadius:50
-        }}>
+        <TouchableOpacity style={styles.button}>
           <Text style={{color:"#ffffff"}}>Click Me</Text>
         </TouchableOpacity>
-      </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  button : {
+    backgroundColor:'#c62828',
+    paddingVertical:20,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:20,
+    marginHorizontal:20,
+    elevation:3,
+    borderRadius:50
+  },
+  textinput : {
+    borderBottomWidth:1, 
+    marginHorizontal: 20, 
+    paddingVertical:10, 
+    borderColor:'#c62828'
+  },
+  switch : {
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:20
+  },
+  image : {
+    width:300,
+    height:300
+  },
+  imageContainer : {
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  textheader : {
+    color:'#FFFFFF',
+    fontWeight:'bold'
+  },
+  textheaderContainer : {
+    backgroundColor:'#c62828',
+    paddingVertical:20,
+    justifyContent: 'center',
+    alignItems:'center',
+    elevation:10
+  }
+})
 
 export default App;

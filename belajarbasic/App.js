a893095a132e316093240657209d8bf5b759a2f0
iervan <iervanfirdiansyah1998@gmail.com>
2022-07-21 19:27:12 +0700

    basic

--- belajarbasic/App.js
+++ belajarbasic/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, Alert, TouchableOpacity} from 'react-native';
 
 let nama = "budi";
+let siswa = [
+  {
+    nama : 'budi',
+    kelas : 1,
+  }
+]
+
+let coba = [1,2,3,4];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
   }
+
+  showAlert = () => {
+    Alert.alert('Hello','World',[{text:'Cancel'},{text:'Ok'}]);
+  }
+
+  hitungLuas = (panjang, lebar) =>{
+    return panjang * lebar;
+  }
+
   render() {
     return (
       <View>
         <Text>
           hello {nama}
         </Text>
+        <Text>
+          hello {siswa.nama}
+        </Text>
+        <Text>
+          hello {siswa.kelas}
+        </Text>
+
+        <Text>{this.hitungLuas(10, 20)}</Text>
+
+        <TouchableOpacity onPress={() => this.showAlert()}>
+          <Text>Test</Text>
+        </TouchableOpacity>
+
+        <FlatList
+          data={siswa}
+          renderItem={({item, index}) => <Text>{item.nama}</Text>}
+          keyExtractor={(item) => item.nama}
+        />
       </View>
     );
   }
